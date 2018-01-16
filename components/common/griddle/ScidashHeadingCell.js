78cf6cd3c85ac56845873a6097288536dc5ef168
NeZanyat <rlatyshenko.dev@gmail.com>
2018-01-16 16:42:28 +0200

    Some UI for start

--- components/common/griddle/ScidashHeadingCell.js
+++ components/common/griddle/ScidashHeadingCell.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import BackendService from '../common/BackendService';
 
 
 export default class ScidashHeadingCell extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.parent = props.parent;
-        this.columnId = props.columnId;
+        this.filterName = props.filterName;
         this.state = {value: ''};
+
+        this.style = {
+            width: "100px"
+        }
     }
 
     render(){
         return (
             <span>
-                {this.props.title}
+                <p>{this.props.title}</p>
                 <input type="text"
+                    style={this.style}
                     value={this.state.value}
                     onChange={(event) => {
                             const value = event.target.value;
-                            this.parent.onFilter(value, this.columnId);
+                            this.parent.onFilter(value, this.filterName);
                             this.setState({value: value})
-                        }
-                    }
+                        }}
             />
             </span>
         )
