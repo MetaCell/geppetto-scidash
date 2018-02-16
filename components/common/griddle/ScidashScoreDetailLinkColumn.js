740b09eea686dccc41e60c8a9fe68cc599bee260
NeZanyat <rlatyshenko.dev@gmail.com>
2018-02-16 14:26:24 +0200

    Color mapping for score

--- components/common/griddle/ScidashScoreDetailLinkColumn.js
+++ components/common/griddle/ScidashScoreDetailLinkColumn.js
@@ -16,19 +16,44 @@ export default class ScidashScoreDetailLinkColumn extends React.Component {
         this.closeScoreDetail = this.closeScoreDetail.bind(this);
         this.state = {
             open: false,
-            scoreObject: new Map()
+            scoreObject: new Map(),
+            colorBlind: false
         };
+    }
+
+    getBackground(){
+        let sortKey = this.state.scoreObject.get("sort_key")
+        let sortKeyRounded = null;
+        let percents = 0;
+        let badValue = 255;
+        let goodValue = 255;
+
+        if (typeof sortKey != "undefined"){
+            sortKeyRounded = sortKey.toFixed(2);
+            percents = sortKeyRounded * 100;
+            badValue = Math.floor(255 - (255 / 100 * percents));
+            goodValue = Math.floor(255 / 100 * percents);
+
+            if (!this.state.colorBlind){
+                return "rgba("+ badValue +", "+ goodValue +", 0, 1)"
+            } else {
+                return "rgba("+ badValue +", "+ badValue +", "+ goodValue +", 1)"
+            }
+        }
+
     }
 
     componentDidMount(){
         this.setState({
-            scoreObject: this.props.value
+            scoreObject: this.props.value,
+            colorBlind: this.props.parent.state.colorBlind
         })
     }
 
     componentWillReceiveProps(nextProps, nextState){
         this.setState({
-            scoreObject: nextProps.value
+            scoreObject: nextProps.value,
+            colorBlind: this.props.parent.state.colorBlind
         });
     }
 
@@ -63,24 +88,40 @@ export default class ScidashScoreDetailLinkColumn extends React.Component {
             score_type = this.state.scoreObject.get("score_type");
         }
 
-            return (
-                <div style={{textAlign: "right", position:"relative", right: "20px"}}>
-                <a
-                onClick={this.openScoreDetail}
-                style={{
-                    cursor: "pointer"
-                }}
-                >{score}</a>
-                <Dialog
-                title={score_type + " details"}
-                actions={actions}
-                modal={true}
-                contentStyle={customContentStyle}
-                open={this.state.open}
-                >
-                This is the details of model {score_type}
-                </Dialog>
+        return (
+            <div>
+                <div style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    background: this.getBackground(),
+                    bottom: "0px",
+                    right: "12px"
+                }}>
                 </div>
-            );
+                <div style={{
+                    textAlign: "right",
+                    position:"relative",
+                    right: "20px"
+                }}>
+                    <a
+                        onClick={this.openScoreDetail}
+                        style={{
+                            cursor: "pointer",
+                            color: "white"
+                        }}
+                    >{score}</a>
+                    <Dialog
+                        title={score_type + " details"}
+                        actions={actions}
+                        modal={true}
+                        contentStyle={customContentStyle}
+                        open={this.state.open}
+                    >
+                    This is the details of model {score_type}
+                    </Dialog>
+                </div>
+        </div>
+        );
     }
 }
