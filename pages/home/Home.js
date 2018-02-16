5c7a4ab79e0a5f779df5ac463e544799d1b053d3
johnidol <giovanni.idili@gmail.com>
2018-02-16 17:20:49 +0000

    override material-ui default theme

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import TestInstances from '../../components/TestInstances.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { grey500, blueGrey900, grey400 } from 'material-ui/styles/colors';
+
+const customTheme = {
+    palette: { 
+      primary1Color: grey500,
+      primary2Color: blueGrey900,
+      primary3Color: grey400,
+      pickerHeaderColor: grey400,
+    }
+};
+  
+const theme = getMuiTheme(customTheme);
 
 export class Home extends React.Component {
-
     render() {
         return (
             <div>
-                <MuiThemeProvider>
+                <MuiThemeProvider muiTheme={theme}>
                     <TestInstances/>
                 </MuiThemeProvider>
             </div>
