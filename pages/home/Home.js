c73d19ef04a9a9f285440e1b3a6836d0aea3b084
johnidol <giovanni.idili@gmail.com>
2018-02-16 17:22:17 +0000

    added link with list of props

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { grey500, blueGrey900, grey400 } from 'material-ui/styles/colors';
 
+// list of props here --> https://github.com/mui-org/material-ui/blob/master/src/styles/baseThemes/lightBaseTheme.js
 const customTheme = {
     palette: { 
       primary1Color: grey500,
