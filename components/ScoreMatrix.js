2e4981f1ca358ddf02f2bf7f900aee4e816673aa
NeZanyat <rlatyshenko.dev@gmail.com>
2018-05-15 15:12:45 +0300

    Matrix View

--- components/ScoreMatrix.js
+++ components/ScoreMatrix.js
@@ -12,7 +12,8 @@ export default class ScoreMatrix extends React.Component {
         this.helper = new Helper();
 
         this.state = {
-            scoreMatrix: {}
+            scoreMatrix: {},
+            hiddenModels: []
         }
     }
 
@@ -31,16 +32,29 @@ export default class ScoreMatrix extends React.Component {
         })
     }
 
+    hideRow(modelName){
+        if (this.state.hiddenModels.includes(modelName))
+            return
+
+        this.setState({
+            hiddenModels: [modelName, ...this.state.hiddenModels]
+        });
+    }
+
     buildMatrix(scoreList = []){
         let scoreMatrix = {
-            headings: {},
+            headings: [],
             rows: {}
         };
 
         for (let score of scoreList){
-            scoreMatrix.headings[score.test_instance.id] = score.test_instance.test_class.class_name;
             let modelName = score.model_instance.model_class.class_name;
             let testName = score.test_instance.test_class.class_name;
+
+            console.log(this.state);
+            if (this.state.hiddenModels.includes(modelName)){
+                continue;
+            }
 
             if (!(modelName in scoreMatrix.rows))
                 scoreMatrix.rows[modelName] = new Map()
@@ -48,7 +62,17 @@ export default class ScoreMatrix extends React.Component {
             if (!(scoreMatrix.rows[modelName].has(`${testName.replace(" ", "_")}_${score.id}`)))
                 scoreMatrix.rows[modelName].set(`${testName.replace(" ", "_")}_${score.id}`, null);
 
-            scoreMatrix.rows[modelName].set(`${testName.replace(" ", "_")}_${score.id}`, score.sort_key);
+            scoreMatrix.rows[modelName].set(`${testName.replace(" ", "_")}_${score.id}`, score);
+        }
+
+        let biggestRow = new Map();
+        for (let row of Object.values(scoreMatrix.rows)){
+            if (row.size > biggestRow.size)
+                biggestRow = row;
+        }
+
+        for (let item of biggestRow.entries()){
+            scoreMatrix.headings.push(item[1].test_instance.test_class.class_name);
         }
 
         this.setState({
@@ -57,28 +81,28 @@ export default class ScoreMatrix extends React.Component {
     }
 
     render(){
-        console.log(this.state);
         return (
             <table className="table">
                 <thead>
                     <tr>
                         <th></th>
-                        {this.state.scoreMatrix.headings && Object.values(this.state.scoreMatrix.headings).map((heading, index) => <th key={index}>{heading}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.scoreMatrix.rows && Object.keys(this.state.scoreMatrix.rows).map((key, index) => {
-                        return (<tr key={index}>
-                            <td>{key}</td>
-                        {[...this.state.scoreMatrix.rows[key]].map((score, index) => <td style={{
-                            background: this.helper.getBackground(score[1], this.props.colorBlind),
-                            color: "white"
-                        }} key={score[0]}>{score[1].toFixed(2)}</td> )}
-                        </tr>)
-                    })}
-                </tbody>
-            </table>
+                    {this.state.scoreMatrix.headings && this.state.scoreMatrix.headings.map((heading, index) => <th key={index}>{heading}</th>)}
+                        <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.scoreMatrix.rows && Object.keys(this.state.scoreMatrix.rows).map((key, index) => {
+                    return (<tr key={index}>
+                        <td>{key}</td>
+                    {[...this.state.scoreMatrix.rows[key]].map((score, index) => <td style={{
+                        background: this.helper.getBackground(score[1].sort_key, this.props.colorBlind),
+                        color: "white"
+                    }} key={score[0]}>{score[1].sort_key.toFixed(2)}</td> )}
+                <td style={{padding: 0}}><i onClick={() => this.hideRow(key)} className="fa fa-eye eye-icon"></i></td>
+            </tr>)
+                })}
+            </tbody>
+        </table>
         )
     }
 }
-
