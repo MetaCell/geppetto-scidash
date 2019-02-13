15f87f3222d6b50d034fbf188d8f824451f76f27
Jesus Martinez <yeshuarmartinez@gmail.com>
2019-02-13 14:41:47 -0600

    save work in progress

--- components/page/header/Header.js
+++ components/page/header/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
 import RaisedButton from "material-ui/RaisedButton";
+import FontIcon from "material-ui/FontIcon";
 import DrawerContainer from "../Drawer/DrawerContainer";
 import PagesService from "../../../services/PagesService";
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Download from 'material-ui/svg-icons/file/file-download';
+
 
 export default class Header extends React.Component {
 
   constructor (props, context){
     super(props, context);
-
+    this.state = {
+    	open: false,
+    };
     this.props = props;
     this.wrapperSettings = null;
 
     this.wrapSettingsRef = this.wrapSettingsRef.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
 
   componentWillMount (){
@@ -20,6 +30,22 @@ export default class Header extends React.Component {
 
   componentWillUnmount (){
     document.removeEventListener("mousedown", event => this.props.handleClickOutsideSettings(this.wrapperSettings, event, this.props.showSettings));
+  }
+  
+  handleTouchTap(event){
+	    // This prevents ghost click.
+	    event.preventDefault();
+
+	    this.setState({
+	      open: true,
+	      anchorEl: event.currentTarget,
+	    });
+  }
+
+  handleRequestClose(){
+	    this.setState({
+	      open: false,
+	    });
   }
 
   wrapSettingsRef (node){
@@ -61,6 +87,7 @@ export default class Header extends React.Component {
     else if (activePage == pagesService.SCHEDULING_PAGE){
       title = "Scheduling";
     }
+    
     return (
       <div id="header">
         <div id="scidash-logo">
@@ -70,14 +97,29 @@ export default class Header extends React.Component {
         <div id="headerLinks">
           <div className="row">
 
-            { this.props.userInfo.isLogged ?
+            { this.props.userInfo.isLogged && this.props.userInfo.userObject.username!=undefined?
               <div className="col-md-3 auth-links">
-                <RaisedButton href="/auth/login" className="userButton loginButton" label={" " + this.props.userInfo.userObject.username} style={{
-                    marginRight: "10px"
-                }}/>
+                <RaisedButton className="userButton loggedButton" 
+                	label={this.props.userInfo.userObject.username.chartAt(0)}
+                	onTouchTap={this.handleTouchTap}
+                	style={{
+                		marginRight: "10px"
+                }}><FontIcon className={"fa fa-user loggedIcon"}></FontIcon></RaisedButton>
                 <RaisedButton href="/auth/logout" className="userButton logoutButton" label="Logout" style={{
                     marginRight: "10px"
                 }}/>
+                <Popover
+                open={this.state.open}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                onRequestClose={this.handleRequestClose}
+              >
+                <Menu>
+                  <MenuItem primaryText={this.props.userInfo.userObject.username} />
+                  <MenuItem primaryText="Logout" leftIcon={<Download />} />
+                </Menu>
+              </Popover>
               </div>
               :
               <div className="col-md-3 auth-links">
