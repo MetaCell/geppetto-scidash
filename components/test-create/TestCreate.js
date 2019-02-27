ebfe3a0691a4abd56de74f4939f8c257dc953baa
Ромчик <romchik@MacBook-Air-Kseniia.local>
2019-02-27 21:45:24 +0530

    Cancel button, and observation sources wiped

--- components/test-create/TestCreate.js
+++ components/test-create/TestCreate.js
@@ -4,7 +4,6 @@ import TestForm from "../test-form/TestForm";
 const TestCreate = ({ model, testClasses, toggleTestForm, onSave, onCancel }) => (<TestForm
   testClasses={testClasses}
   model={model}
-  toggleTestForm={toggleTestForm}
   onSave={onSave}
   onCancel={onCancel}
 />
