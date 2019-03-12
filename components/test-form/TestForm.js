b00cf532c02cc3f5cdb55b7a4f630ca9fd8b3757
Jesus Martinez <yeshuarmartinez@gmail.com>
2019-03-12 17:23:42 -0600

    saving work in progress

--- components/test-form/TestForm.js
+++ components/test-form/TestForm.js
@@ -56,12 +56,24 @@ export default class TestForm extends React.Component {
           />
 
           <SelectField
+            id="selectField"
             style={styles.firstLine.two}
             iconStyle={styles.firstLine.icon}
             value={this.state.model.test_class.id}
             floatingLabelText="Select test class"
             floatingLabelFixed={false}
+            canAutoPosition={true}
             underlineStyle={{ borderBottom: "1px solid grey" }}
+          	dropDownMenuProps={{
+          		 menuStyle:{
+          			 border: "1px solid black",
+          			 backgroundColor: '#f5f1f1'
+                 },
+                 anchorOrigin:{
+                	 vertical:"center",
+                	 horizontal:"left"
+                  }
+            }}
             onChange={(e, key, value) => {
               for (let klass of this.state.testClasses) {
                 if (klass.id == value) {
