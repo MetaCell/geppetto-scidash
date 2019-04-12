b6f6f7795b0d9ab24dcf23370b0b74a91c483481
Dario Del Piano <dario@metacell.us>
2019-04-12 11:48:40 +0100

    update and getInstanceId service calls defined to update and retrieve an instance with the backend

--- services/api/ModelsApiService.js
+++ services/api/ModelsApiService.js
@@ -1,7 +1,39 @@
 import ApiService from "./ApiService";
+import FilteringService from "../FilteringService";
 
 export default class ModelsApiService extends ApiService {
 
     endpoint = "/api/model-instances/"
+
+    getModelId(id = null, cache = false, namespace = "") {
+        let filteringS = FilteringService.getInstance();
+
+        if (this.endpoint === null) {
+            throw new ApiServiceException("You should define API endpoint");
+        }
+
+        if (id === null || id === undefined) {
+            throw new ApiServiceException("A valid ID should be provided");
+        }
+
+        let queryPath = this.endpoint + id + "/?format=json";
+
+        if (this.storage.getItem(queryPath) && cache) {
+            return new Promise(resolve => {
+                resolve(this.getFromCache(queryPath));
+            });
+        }
+
+        let request = fetch(queryPath).then(result => result.json());
+
+        if (cache) {
+            return request.then(result => {
+                this.saveToCache(queryPath, result);
+                return result;
+            });
+        } else {
+            return request;
+        }
+    }
 
 }
