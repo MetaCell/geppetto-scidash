1cd1ca23563f7cfa209793f68865a9ca5305073b
Roman <roman@Pro-15.local>
2019-04-16 15:34:16 +0300

    Small hot fixes

--- components/score-details/ScoreDetailsContainer.js
+++ components/score-details/ScoreDetailsContainer.js
@@ -5,45 +5,54 @@ import Helper from "../../shared/Helper";
 
 const mapStateToProps = (state, ownProps) => {
 
-    let helper = new Helper();
-    let sortKey = ownProps.score.get("sort_key");
+  let helper = new Helper();
 
-    let timestamp = new Date(ownProps.score.get("timestamp")).toLocaleString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZone: "UTC",
-        timeZoneName: "short"
-    });
+  let sortKey = "N/A";
 
-    let background = helper.getBackground(sortKey, ownProps.colorBlind);
+  if (ownProps.score.get("score_key") !== null){
+    sortKey = ownProps.score.get("sort_key");
+  }
 
-    return {
-        scoreClassName: helper.noneIfEmptyString(ownProps.score.get("test_instance").get("test_class").get("class_name")),
-        testClassName: helper.noneIfEmptyString(ownProps.score.get("test_instance").get("test_class").get("class_name")),
-        score: ownProps.score.get("score").toFixed(3),
-        scoreType: ownProps.score.get("score_type"),
-        hostname: helper.noneIfEmptyString(ownProps.score.get("hostname")),
-        buildInfo: helper.parseBuildInfo(ownProps.score.get("test_instance").get("build_info")),
-        timestamp: timestamp,
-        modelBackend: helper.noneIfEmptyString(ownProps.score.get("model_instance").get("backend")),
-        observation: helper.noneIfEmptyMap(ownProps.score.get("test_instance").get("observation")),
-        modelInstance: ownProps.score.get("model_instance"),
-        sortKey: sortKey.toFixed(2),
-        background
-    }
-}
+  let score = "N/A";
 
-const mapDispatchToProps = dispatch => {
-    return {}
-}
+  if (ownProps.score.get("score") !== null){
+    score = ownProps.score.get("score");
+  }
+
+  let timestamp = new Date(ownProps.score.get("timestamp")).toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZone: "UTC",
+    timeZoneName: "short"
+  });
+
+  let background = helper.getBackground(sortKey, ownProps.colorBlind);
+
+  return {
+    scoreClassName: helper.noneIfEmptyString(ownProps.score.get("test_instance").get("test_class").get("class_name")),
+    testClassName: helper.noneIfEmptyString(ownProps.score.get("test_instance").get("test_class").get("class_name")),
+    score: score,
+    scoreType: ownProps.score.get("score_type") || ownProps.score.get("score_class").get("class_name"),
+    hostname: helper.noneIfEmptyString(ownProps.score.get("hostname")),
+    buildInfo: helper.parseBuildInfo(ownProps.score.get("test_instance").get("build_info")),
+    timestamp: timestamp,
+    modelBackend: helper.noneIfEmptyString(ownProps.score.get("model_instance").get("backend")),
+    observation: helper.noneIfEmptyMap(ownProps.score.get("test_instance").get("observation")),
+    modelInstance: ownProps.score.get("model_instance"),
+    sortKey: sortKey,
+    background
+  };
+};
+
+const mapDispatchToProps = dispatch => ({});
 
 const ScoreDetailsContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ScoreDetails)
+  mapStateToProps,
+  mapDispatchToProps
+)(ScoreDetails);
 
 export default ScoreDetailsContainer;
