2d4fda9e0dd62934bc329d0fb7cce38a208022e1
Zoran Sinnema <zsinnema@gmail.com>
2020-03-19 12:02:32 -0500

    feat: fix issues with datefilter reset

--- actions/scores.js
+++ actions/scores.js
@@ -39,9 +39,10 @@ export function dateFilterChanged (state, action){
 export function dateFilterClear (state, action){
   let filteringService = FilteringService.getInstance();
 
-  filteringService.restoreFromInitial(Config.scoresNamespace);
+  filteringService.restoreFromInitial(Config.globalNamespace, 'timestamp_from');
+  filteringService.restoreFromInitial(Config.globalNamespace, 'timestamp_to');
 
-  for (let entry of Object.entries(filteringService.getFilters(Config.scoresNamespace))){
+  for (let entry of Object.entries(filteringService.getFilters(Config.globalNamespace))){
     action.filter(entry[1], entry[0], action.dispatch, true);
   }
 
