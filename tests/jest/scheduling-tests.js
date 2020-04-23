8efeffd0d76ec280c6913efcbc4d1f91c965ea54
jrmartin <yeshuarmartinez@gmail.com>
2020-04-23 07:55:40 -0700

    Reset timeout in scheduling-tests

--- tests/jest/scheduling-tests.js
+++ tests/jest/scheduling-tests.js
@@ -47,7 +47,7 @@ var tableModelLength = 2;
  */
 describe('Scidash Scheduling Tests', () => {
 	beforeAll(async () => {
-		jest.setTimeout(180000);
+		jest.setTimeout(125000);
 		await page.setViewport({ width: 1280, height: 800 })
 		await page.goto(scidashURL);
 	});
